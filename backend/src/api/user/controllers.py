d4615922bd2dd2edc616749b5e9f7dc296c4148e
Aliaksandr Zubchyk <sanch13sanch@gmail.com>
2025-10-09 18:43:38 +0300

    add env example temporary add bg task to controller edit dc add 2 bg task (1 bg task and 1 periodic task)

--- backend/src/api/user/controllers.py
+++ backend/src/api/user/controllers.py
@@ -1,3 +1,4 @@
+import os
 from uuid import UUID
 
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
@@ -18,12 +19,15 @@ async def get_by_id(user_id: UUID, use_case: FromDishka[UserGetByIdUseCase]) -> 
     return UserApiMapper.dto_to_schema(dto_out)
 
 
-# TODO установить ResponseSchema
 @router.post("/", status_code=201, response_model=UserResponseSchema)
 async def create(
     user_data: UserCreateSchema,
     use_case: FromDishka[UserCreateUseCase],
 ) -> UserResponseSchema:
     dto_out = await use_case.execute(UserApiMapper.schema_to_dto(user_data))
-    await send_email_task.kiq(data=dto_out.email)
+    email_to = [os.getenv("SMTP_EMAIL_TO")]
+    await send_email_task.kiq(
+        data=f"Рег пользак с мылом: {dto_out.email}",
+        email_to=email_to,
+    )
     return UserApiMapper.dto_to_schema(dto_out)
--- backend/src/core/bg_tasks/tasks.py
+++ backend/src/core/bg_tasks/tasks.py
@@ -1,37 +1,34 @@
+# import os
 from email.message import EmailMessage
 
 from src.config.smtp_settings import smtp_config
 from src.core.bg_tasks.redis_broker import broker
 from src.core.bg_tasks.utils import connect_smtp_and_send_email
 
-
-@broker.task
-async def example_task(name: str) -> str:
-    print(f"Hello, {name}!")
-    return f"Hello, {name}!"
-
-
-@broker.task(schedule=[{"cron": "*/1 * * * *"}])  # Каждые 1 минут
-async def scheduled_task():
-    print("🕒 Scheduled task started!")  # Check if this appears in the worker logs
-    await send_email_task.kiq("This task runs every 1 minute")
-    print("✅ Scheduled task completed")
-    return "Scheduled task completed"
+# Оставлен как пример периодик задачи
+# @broker.task(schedule=[{"cron": "*/1 * * * *"}])  # Каждые 1 минут
+# async def scheduled_task():
+#     await send_email_task.kiq(
+#         data="This task runs every 1 minute",
+#         email_to=[os.getenv("SMTP_EMAIL_TO")]
+#     )
+#     return "Scheduled task completed"
 
 
 @broker.task
-async def send_email_task(data: str) -> None:
+async def send_email_task(data: str, email_to: list[str]) -> None:
     message = EmailMessage()
     message["Subject"] = "subject"
     message["From"] = smtp_config.user
-    message["To"] = "a.zubchyk@miran-bel.com"
+    message["To"] = email_to
 
     message.set_content(data)
 
+    # Оставлен как пример добавления HTML-контент в письмо
     # message.set_content(data, subtype='html')  # Устанавливаем HTML-контент
     # Добавляем plain-text версию для клиентов, не поддерживающих HTML
     # message.add_alternative(data, subtype='plain')
-
+    # Оставлен как пример прикрепления файла в письмо
     # filename = data.get("filename")
     # message.add_attachment(file_stream.read(),
     #                        maintype='application',
