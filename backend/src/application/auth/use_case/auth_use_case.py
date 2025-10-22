b04fcc49cd3349bb88cc05d38b5fbb06af915070
sanch <sanch13sanch@gmail.com>
2025-10-22 15:37:01 +0300

    add TokenOutSchema refactor login_user refactor AuthUserUseCase

--- backend/src/application/auth/use_case/auth_use_case.py
+++ backend/src/application/auth/use_case/auth_use_case.py
@@ -1,4 +1,3 @@
-from src.application.auth.exceptions import UnauthorizedError
 from src.application.auth.services.auth_user_service import AuthenticateUserService
 from src.application.exception_decorator import handle_db_errors
 from src.domain.user.dtos import UserAuthInputDto
@@ -16,6 +15,7 @@ class AuthUserUseCase:
 
     @handle_db_errors
     async def execute(self, dto: UserAuthInputDto) -> dict:
+    async def execute(self, dto: UserAuthInputDto, meta: dict) -> dict:
         user_entity = await self.auth_service.authenticate_user(dto)
 
         if user_entity is None:
@@ -23,9 +23,13 @@ class AuthUserUseCase:
 
         access_token = await self.jwt_service.create_access_token(user_entity)
         refresh_token = await self.jwt_service.create_refresh_token(user_entity)
+        expires_in = self.jwt_service.settings.ACCESS_TOKEN_LIFETIME_MINUTES * 60
+        expires_in_refresh = self.jwt_service.settings.REFRESH_TOKEN_LIFETIME_DAYS * 24 * 3600
 
-        return {
-            "message": "Добро пожаловать в систему",
+        dict_ = {
+            "expires_in": expires_in,
+            "expires_in_refresh": expires_in_refresh,
             "access_token": access_token,
             "refresh_token": refresh_token,
         }
+        return dict_
